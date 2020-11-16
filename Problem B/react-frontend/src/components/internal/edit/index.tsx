424ab0666ff4f72f225b6ff70aa5782bdc6dd05c
oberruti <54275024+oberruti@users.noreply.github.com>
2020-11-16 16:58:22 -0300

    pages in progress

--- Problem B/react-frontend/src/components/internal/edit/index.tsx
+++ Problem B/react-frontend/src/components/internal/edit/index.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import { VerticalStack } from 'common/components/flex'
+import { InternalLayout } from 'components/app/internalLayout'
+import React, { useState } from 'react'
 import { Cookies } from 'react-cookie/lib'
+import { backgroundColor } from 'style'
+import { Style } from 'utils/tsTypes'
 
 export const InternalEditAppointment = (props: { cookies: Cookies }): JSX.Element => {
-    return <div>edit</div>
+    const [email, setEmail] = useState('')
+    const style: Style = {
+        background: backgroundColor,
+        display: 'flex',
+        alignItems: 'center',
+        position: 'absolute',
+        textAlign: 'center',
+        verticalAlign: 'middle',
+        width: '100%',
+        height: '100%',
+        overflowY: 'hidden',
+        alignContent: 'center',
+        marginTop: '30px',
+            fontStyle: 'normal',
+            fontWeight: 'normal',
+            fontSize: '20px',
+            fontFamily: 'New York Medium',
+    }
+    return (
+        <InternalLayout cookies={props.cookies}>
+            <VerticalStack style={style}>
+                This page is still in progress.
+            </VerticalStack>
+        </InternalLayout>
+    )
 }
